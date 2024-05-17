753cc2cff7a4ebeef080ae8a45ba38b3c8251923
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-05-17 11:04:56 -0700

    import browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { Button } from "@chakra-ui/react"
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/HomePage/HomePage"
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />}/>
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
+import { BrowserRouter } from "react-router-dom";
 
 const styles = {
   global: (props) => ({
