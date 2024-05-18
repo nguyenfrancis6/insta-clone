563751022095fa69cf5893a4ac09b5fa32a1ffa4
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-05-18 14:29:06 -0700

    Fully created FeedPost, passed feedpost props

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import { Box, Flex, Text, InputGroup, InputRightElement, Input, Button } from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  CommentLogo,
+  NotificationsLogo,
+  UnlikeLogo,
+} from "../../assets/constants";
 
-const PostFooter = () => {
+const PostFooter = ({ username }) => {
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(1000);
+
+  const handleLike = () => {
+    if (liked) {
+      setLiked(false);
+      setLikes(likes - 1);
+    } else {
+      setLiked(true);
+      setLikes(likes + 1);
+    }
+  };
+
   return (
-    <div>PostFooter</div>
-  )
-}
+    <Box mb={10}>
+      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
+        <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
+          {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
+        </Box>
 
-export default PostFooter
+        <Box cursor={"pointer"} fontSize={18}>
+          <CommentLogo />
+        </Box>
+      </Flex>
+      <Text fontWeight={600} fontSize={"sm"}>
+        {likes} likes
+      </Text>
+      <Text fontSize="sm" fontWeight={700}>
+        {username} {" "}
+        <Text fontWeight={400} as="span">
+          Feeling good
+        </Text>
+      </Text>
+      <Text color={"gray"} fontSize={"sm"}>
+        View all 1,000 comments
+      </Text>
+      <Flex
+        alignItems={"center"}
+        gap={2}
+        justifyContent={"space-between"}
+        w={"full"}
+      >
+        <InputGroup>
+          <Input
+            variant={"flushed"}
+            placeholder={"Add a comment..."}
+            fontSize={14}
+          />
+          <InputRightElement>
+            <Button
+              fontSize={14}
+              color={"blue.500"}
+              fontWeight={600}
+              cursor={"pointer"}
+              _hover={{ color: "white" }}
+              bg={"transparent"}
+            >
+              Post
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </Flex>
+    </Box>
+  );
+};
+
+export default PostFooter;
