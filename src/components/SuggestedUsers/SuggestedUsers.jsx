769437d49ef4e081ba398ccc0d6bdd8e605fcb80
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-05-18 16:08:04 -0700

    Suggested user section

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,14 +1,52 @@
-import SuggestedUser from "./SuggestedUser"
-import SuggestedHeader from "./SuggestedHeader"
-import { VStack } from "@chakra-ui/react"
+import SuggestedUser from "./SuggestedUser";
+import SuggestedHeader from "./SuggestedHeader";
+import { VStack, Flex, Text, Box, Link } from "@chakra-ui/react";
 
 const SuggestedUsers = () => {
   return (
     <VStack py={8} px={6} gap={4}>
-      <SuggestedHeader /> 
-
+      <SuggestedHeader />
+      <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+        <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+          Suggested for you
+        </Text>
+        <Text
+          fontSize={12}
+          fontWeight={"bold"}
+          _hover={{ color: "gray.400" }}
+          cursor={"pointer"}
+        >
+          See All
+        </Text>
+      </Flex>
+      <SuggestedUser
+        name="Dan Abrahmov"
+        followers={1392}
+        avatar="https://bit.ly/dan-abramov"
+      />
+      <SuggestedUser
+        name="Ryan Florence"
+        followers={567}
+        avatar="https://bit.ly/ryan-florence"
+      />
+      <SuggestedUser
+        name="Christian Nwamba"
+        followers={759}
+        avatar="https://bit.ly/code-beast"
+      />
+      <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+        © 2024 Built by{" "}
+        <Link
+          href="https://github.com/nguyenfrancis6"
+          target="_blank"
+          color="blue.500"
+          fontSize={14}
+        >
+          Francis Nguyen
+        </Link>
+      </Box>
     </VStack>
-  )
-}
+  );
+};
 
-export default SuggestedUsers
+export default SuggestedUsers;
