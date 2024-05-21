77f586eccbfbe9c289431d006a76569eff40173a
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-05-20 21:46:43 -0700

    Profile posts + skeleton, modal boilerplate

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import { GridItem, Flex, Text, Image, useDisclosure, Modal, ModalContent, ModalOverlay, ModalHeader, ModalBody, ModalCloseButton } from "@chakra-ui/react";
+import { AiFillHeart } from "react-icons/ai";
+import { FaComment } from "react-icons/fa";
 
-const ProfilePost = () => {
+const ProfilePost = ({ img }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <div>ProfilePost</div>
-  )
-}
+    <>
+      <GridItem
+        cursor={"pointer"}
+        borderRadius={4}
+        overflow={"hidden"}
+        border={"1px solid"}
+        borderColor={"whiteAlpha.300"}
+        position={"relative"}
+        aspectRatio={1 / 1}
+        onClick={onOpen}
+      >
+        <Flex
+          opacity={0}
+          _hover={{ opacity: 1 }}
+          position={"absolute"}
+          top={0}
+          right={0}
+          left={0}
+          bottom={0}
+          bg={"blackAlpha.700"}
+          transition={"all 0.3s ease"}
+          zIndex={1}
+          justifyContent={"center"}
+        >
+          <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
+            <Flex>
+              <AiFillHeart size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                7
+              </Text>
+            </Flex>
+            <Flex>
+              <FaComment size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                7
+              </Text>
+            </Flex>
+          </Flex>
+        </Flex>
 
-export default ProfilePost
+        <Image
+          src={img}
+          alt="profile post"
+          w={"100%"}
+          h={"100%"}
+          objectFit={"cover"}
+        />
+      </GridItem>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Modal sentence
+          </ModalBody>
+
+
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ProfilePost;
--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { Box, Grid, VStack, Skeleton } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import ProfilePost from "./ProfilePost";
 
 const ProfilePosts = () => {
-  return (
-    <div>ProfilePosts</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(true);
+  
 
-export default ProfilePosts
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <Grid
+      templateColumns={{ sm: "repeat(1, 1fr)", md: "repeat(3, 1fr)" }}
+      gap={1}
+      columnGap={1}
+    >
+      {isLoading &&
+        [0, 1, 2, 3, 4, 5].map((_, idx) => (
+          <VStack key={idx} alignItems={"flex-start"} gap={4}>
+            <Skeleton w={"full"}>
+              <Box h="300px">Content wrapped</Box>
+            </Skeleton>
+          </VStack>
+        ))}
+
+      {!isLoading && (
+        <>
+          <ProfilePost img='/img1.png'/>
+          <ProfilePost img='/img2.png'/>
+          <ProfilePost img='/img3.png'/>
+          <ProfilePost img='/img4.png'/>
+        </>
+      )}
+    </Grid>
+  );
+};
+
+export default ProfilePosts;
