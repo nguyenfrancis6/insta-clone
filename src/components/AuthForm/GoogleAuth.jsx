93908312d73a61d0c5774c138f957a2b3263a774
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-05-21 21:31:10 -0700

    Login feature + header log out/profile

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -1,13 +1,13 @@
 import { Flex, Image, Text } from "@chakra-ui/react";
 
-const GoogleAuth = () => {
-  
+const GoogleAuth = ({prefix}) => {
+
 
   return (
     <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}>
       <Image src="/google.png" w={5} alt="Google logo" />
       <Text mx={2} color={"blue.500"}>
-        Log in with Google
+        {prefix} with Google
       </Text>
     </Flex>
   );
--- src/components/AuthForm/Login.jsx
+++ src/components/AuthForm/Login.jsx
@@ -1,5 +1,6 @@
-import { Input, Button } from "@chakra-ui/react";
+import { Input, Button, Alert, AlertIcon } from "@chakra-ui/react";
 import { useState } from "react";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
   const [inputs, setInputs] = useState({
@@ -7,13 +8,15 @@ const Login = () => {
     password: "",
   });
 
+  const {loading, error, login} = useLogin();
+
   return (
     <>
       <Input
         placeholder="Email"
         fontSize={14}
         type="email"
-        size={'sm'}
+        size={"sm"}
         value={inputs.email}
         onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
       />
@@ -21,11 +24,25 @@ const Login = () => {
         placeholder="Password"
         fontSize={14}
         type="password"
-        size={'sm'}
+        size={"sm"}
         value={inputs.password}
         onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
       />
-      <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14}>
+
+      {error && (
+        <Alert status="error" fontSize={13} p={2} borderRadius={4}>
+          <AlertIcon fontSize={12} />
+          {error.message}
+        </Alert>
+      )}
+      <Button
+        w={"full"}
+        colorScheme="blue"
+        size={"sm"}
+        fontSize={14}
+        isLoading={loading}
+        onClick={() => login(inputs)}
+      >
         Log in
       </Button>
     </>
