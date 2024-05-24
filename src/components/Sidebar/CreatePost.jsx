0ae4f4c0c9506a93bb3d2ad6ce7322784570759f
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-05-23 23:03:20 -0700

    Added create post feature and update posts on profile page

--- src/components/Sidebar/CreatePost.jsx
+++ src/components/Sidebar/CreatePost.jsx
@@ -18,9 +18,43 @@ import {
 } from "@chakra-ui/react";
 import { CreatePostLogo } from "../../assets/constants";
 import { BsFillImageFill } from "react-icons/bs";
+import { useRef, useState } from "react";
+import usePreviewImg from "../../hooks/usePreviewImg";
+import useAuthStore from "../../store/authStore";
+import usePostStore from "../../store/postStore";
+import useShowToast from "../../hooks/useShowToast";
+import useUserProfileStore from "../../store/userProfileStore";
+import { useLocation } from "react-router-dom";
+import {
+  addDoc,
+  arrayUnion,
+  collection,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+import { db, storage } from "../../firebase/firebase";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 
 const CreatePost = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [caption, setCaption] = useState();
+  const imageRef = useRef(null);
+  const { handleImageChange, selectedFile, setSelectedFile } = usePreviewImg();
+  const { isLoading, handleCreatePost } = useCreatePost();
+  const showToast = useShowToast();
+
+  // set post with image and caption, reset state after
+  const handlePostCreation = async () => {
+    try {
+      await handleCreatePost(selectedFile, caption);
+      onClose();
+      setCaption("");
+      setSelectedFile(null);
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    }
+  };
+
   return (
     <>
       <Tooltip
@@ -52,9 +86,17 @@ const CreatePost = () => {
           <ModalHeader>Create Post</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            <Textarea placeholder="Post caption..." />
+            <Textarea
+              placeholder="Post caption..."
+              onChange={(e) => setCaption(e.target.value)}
+            />
 
-            <Input type="file" hidden />
+            <Input
+              type="file"
+              hidden
+              ref={imageRef}
+              onChange={handleImageChange}
+            />
 
             <BsFillImageFill
               style={{
@@ -63,11 +105,32 @@ const CreatePost = () => {
                 cursor: "pointer",
               }}
               size={16}
+              onClick={() => imageRef.current.click()}
             />
+            {selectedFile && (
+              <Flex
+                mt={5}
+                w={"full"}
+                position={"relative"}
+                justifyContent={"center"}
+              >
+                <Image src={selectedFile} alt="Selected img" />
+                <CloseButton
+                  position={"absolute"}
+                  top={2}
+                  right={2}
+                  onClick={() => {
+                    setSelectedFile(null);
+                  }}
+                />
+              </Flex>
+            )}
           </ModalBody>
 
           <ModalFooter>
-            <Button mr={3}>Post</Button>
+            <Button mr={3} onClick={handlePostCreation} isLoading={isLoading}>
+              Post
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
@@ -76,3 +139,51 @@ const CreatePost = () => {
 };
 
 export default CreatePost;
+
+function useCreatePost() {
+  const showToast = useShowToast();
+  const [isLoading, setIsLoading] = useState(false);
+  const authUser = useAuthStore((state) => state.user);
+  const createPost = usePostStore((state) => state.createPost);
+  const addPost = useUserProfileStore((state) => state.addPost);
+  const { pathname } = useLocation();
+
+  const handleCreatePost = async (selectedFile, caption) => {
+    if (!selectedFile) throw new Error("Please select an image");
+    setIsLoading(true);
+    const newPost = {
+      caption: caption,
+      likes: [],
+      comments: [],
+      createdAt: Date.now(),
+      createdBy: authUser.uid,
+    };
+
+    try {
+      const postDocRef = await addDoc(collection(db, "posts"), newPost);
+      const userDocRef = doc(db, "users", authUser.uid);
+      const imageRef = ref(storage, `posts/${postDocRef.id}`);
+
+      //update posts in db
+      await updateDoc(userDocRef, { posts: arrayUnion(postDocRef.id) });
+      //store image in storage
+      await uploadString(imageRef, selectedFile, "data_url");
+      const downloadURL = await getDownloadURL(imageRef);
+
+      //update posts with saved image from storage
+      await updateDoc(postDocRef, { imageURL: downloadURL });
+
+      // add image url to new post
+      newPost.imageURL = downloadURL;
+      createPost({ ...newPost, id: postDocRef.id });
+      addPost({ ...newPost, id: postDocRef.id });
+      showToast("Success", "Post created successfully", "success");
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { isLoading, handleCreatePost };
+}
--- src/store/userProfileStore.js
+++ src/store/userProfileStore.js
@@ -1,9 +1,16 @@
-import { create } from "zustand" 
+import { create } from "zustand";
 
 const useUserProfileStore = create((set) => ({
-  userProfile:null, 
-  setUserProfile: (userProfile) => set({userProfile}),
-    //addPost: ()
-}))
+  userProfile: null,
+  setUserProfile: (userProfile) => set({ userProfile }),
+  // used to update number of posts in profile page
+  addPost: (post) =>
+    set((state) => ({
+      userProfile: {
+        ...state.userProfile,
+        posts: [post.id, ...state.userProfile.posts],
+      },
+    })),
+}));
 
-export default useUserProfileStore
+export default useUserProfileStore;
